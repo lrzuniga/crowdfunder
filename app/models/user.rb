46ba8f38637bd3afcf7f54ffeb8e17f31fc5e7e7
lrzuniga <lrzuniga@gmail.com>
2013-09-30 14:26:58 -0400

    Seeding db

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
   authenticates_with_sorcery!
-  attr_accessible :first_name, :last_name, :email, :password
+  attr_accessible :first_name, :last_name, :email, :password #, user_id
   has_many :projects
 end
