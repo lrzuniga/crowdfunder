373b67c7083874de01d4458325a2f8a6fc8fd387
lrzuniga <lrzuniga@gmail.com>
2013-09-30 14:34:44 -0400

    Adding FactoryGirl in replacement of fixtures and Capybara for integration specs

--- config/application.rb
+++ config/application.rb
@@ -30,7 +30,10 @@ module Crowdfunder
     #crowdfunder
     config.generators do |g|
       g.test_framework :rspec, :fixture => false
+      g.fixture_replacement :factory_girl, :dir => "spec/factories"
     end
+
+
 end
 end
 
