a61fb1523a1c6f37599809de367735b5e0553251
lrzuniga <lrzuniga@gmail.com>
2013-10-01 19:17:55 -0400

    Writing tests for projects show page

--- spec/helpers/welcome_controller_helper_spec.rb
+++ spec/helpers/welcome_controller_helper_spec.rb
@@ -10,6 +10,6 @@ require 'spec_helper'
 #     end
 #   end
 # end
-describe WelcomeControllerHelper do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+# describe WelcomeControllerHelper do
+#   pending "add some examples to (or delete) #{__FILE__}"
+# end
