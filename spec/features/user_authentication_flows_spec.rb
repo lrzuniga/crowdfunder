da3d0e86d5d6eabea439002a1020d455cffab2df
lrzuniga <lrzuniga@gmail.com>
2013-10-02 16:57:54 -0400

    Writing tests for login and logout

--- spec/features/user_authentication_flows_spec.rb
+++ spec/features/user_authentication_flows_spec.rb
@@ -37,7 +37,7 @@ describe "User Authentication" do
       visit "/users/new"
       user = FactoryGirl.build(:user) 
 
-      # Invalid form submission ... 
+      # Invalid form submission ...
       fill_in "user[email]", :with => user.email
       click_button "Create Account"
       # Should redirect to users_path and ...
@@ -46,6 +46,41 @@ describe "User Authentication" do
 
       page.should have_no_content("Account created")
       expect(page).to have_no_content("Account created")
+    end
+
+    it "should successfully log in" do
+      visit '/'
+      find('.navbar').has_no_link?('Logout').should be_true
+      # Calling the helper method here, and it returns a user
+      user = setup_signed_in_user
+      find('.navbar').has_link?('Logout').should be_true
+    end
+
+    it "should unsuccessfully log in" do
+      visit '/sessions/new'
+
+      fill_in "email", with: "a@b.com"
+      fill_in "password", with: "invalid creds"
+      click_button "Login"
+
+      expect(current_path).to eq(sessions_path)
+
+      page.should have_content('Invalid')
+      expect(page).to have_content('Invalid')
+    end
+
+    it "should successfully logout" do
+      # Calling the helper method again
+      user = setup_signed_in_user
+
+      visit '/'
+
+      find('.navbar').click_link 'Logout'
+
+      page.should have_content("Bye")
+      expect(page).to have_content("Bye")
+
+      find('.navbar').has_no_link?('Logout')
     end
   end
 end
